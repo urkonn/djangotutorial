ddd0b8f45bbb5c64e0f0b5a9d03ed5e55bcf1d5e
Julio Acuña <urkonn@gmail.com>
2013-10-24 02:09:35 -0500

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    (r'^polls/', include('polls.urls', namespace='polls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+)
